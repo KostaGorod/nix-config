619da71ba380fe09af0181cbf7179e44ddd0c645
Kosta Gorod <35299380+KostaGorod@users.noreply.github.com>
2024-11-04 10:22:55 +0200

    pkgs(system): add coreutils

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -238,7 +238,7 @@ services.tlp = {
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     helix
-
+    coreutils
     # GUI
     numix-cursor-theme
     # Virtual Keyboard
