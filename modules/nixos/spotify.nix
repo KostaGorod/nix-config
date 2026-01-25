03dab1d2113f11c36c8dfefdb15f127f91d5cda8
Kosta Gorod <35299380+KostaGorod@users.noreply.github.com>
2026-01-25 10:27:10 +0200

    Revert "refactor: make spotify overlay conditional and lazy"
    This reverts commit 0d7d3251e5b4913dca7702669efe7a2e76fad317.

--- modules/nixos/spotify.nix
+++ modules/nixos/spotify.nix
@@ -1,43 +1,10 @@
-# Spotify with SpotX ad-blocker patch
-{ config, lib, pkgs, ... }:
-
-let
-  cfg = config.programs.spotify-patched;
-  
-  spotify-with-spotx = pkgs.spotify.overrideAttrs (old: 
-    let
-      spotx = pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/SpotX-Official/SpotX-Bash/a814894e8cca3282a0ad79f30896ec4b01544d4e/spotx.sh";
-        hash = "sha256-ZYep5dmg07ae+3kvN+BTKDB2Ky76Xe2fXR3VKKnDMsQ=";
-      };
-    in {
-      nativeBuildInputs = old.nativeBuildInputs ++ (with pkgs; [
-        util-linux perl unzip zip curl
-      ]);
-      
-      unpackPhase = builtins.replaceStrings
-        [ "runHook postUnpack" ]
-        [ ''
-            patchShebangs --build ${spotx}
-            runHook postUnpack
-          '' ]
-        old.unpackPhase;
-      
-      installPhase = builtins.replaceStrings
-        [ "runHook postInstall" ]
-        [ ''
-            bash ${spotx} -f -P "$out/share/spotify"
-            runHook postInstall
-          '' ]
-        old.installPhase;
-    });
-in
+{ pkgs, ... }:
 {
-  options.programs.spotify-patched = {
-    enable = lib.mkEnableOption "Spotify with SpotX ad-blocker patch";
-  };
+  nixpkgs.overlays = [
+    (import ../../overlays/spotify-overlay.nix)
+  ];
 
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ spotify-with-spotx ];
-  };
+  environment.systemPackages = with pkgs; [
+    spotify-with-spotx
+  ];
 }
